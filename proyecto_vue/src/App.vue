d84cec131dbc67a77c8d66cab59d9fe300d5ca72
Adexe <adexe.reyes.cb@gmail.com>
2024-02-15 20:54:31 +0000

    ejercicio terminado

--- proyecto_vue/src/App.vue
+++ proyecto_vue/src/App.vue
@@ -2,34 +2,86 @@
   <div id="app">
     <div v-if="!showCart">
       <div>
-        <UserInput />
+        <div>
+          <!-- Input para el nombre de usuario -->
+          <input type="text" v-model="userName" class="inputUser" placeholder="Insertar nombre de usuario">
+          <!-- Mostrar mensaje de bienvenida solo si el nombre de usuario no está vacío -->
+          <div v-if="userName !== ''" class="welcome-message">Bienvenido {{ userName }}</div>
+        </div>
         <h1>Listado de productos</h1>
-        <button @click="showCart = true, emit('cart-updated', updatedCart)" class="go-to-cart-btn">Ir al carrito ({{
-          cart.length }})</button>
+        <div>
+          <button @click="showCart = true" class="go-to-cart-btn">Ir al carrito ({{ cart.length }})</button>
+          <button @click="sendOrder" class="send-order-btn">Enviar Pedido</button>
+        </div>
       </div>
-      <ProductList :cart="cart" @product-added="addToCart" />
+      <ProductList :cart="cart" />
     </div>
-    <Cart v-if="showCart" :cart="cart" @cart-updated="handleCartUpdate" @back-to-products="showCart = false" />
+    <Cart v-if="showCart" :cart="cart" @back-to-products="showCart = false" />
+    <!-- Botón para eliminar el carrito -->
+    <button @click="removeShoppingCart" class="remove-cart-btn">Eliminar Carrito</button>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import UserInput from './components/UserInput.vue';
+import { ref, watch, onMounted } from 'vue';
 import ProductList from './components/ProductList.vue';
 import Cart from './components/Cart.vue';
 
+const userName = ref('Adexe');
 const cart = ref([]);
 const showCart = ref(false);
 
-// No necesitas pasar cart como argumento a addToCart
-const addToCart = (product) => {
-  cart.value.push(product);
+// Watcher para observar cambios en cart y almacenarlo en local storage
+watch(cart, (newCart) => {
+  localStorage.setItem('cart', JSON.stringify(newCart));
+}, { deep: true });
+
+// Función para cargar el carrito desde el local storage al iniciar la aplicación
+const loadLocalCart = () => {
+  const savedCart = localStorage.getItem('cart');
+  if (savedCart) {
+    cart.value = JSON.parse(savedCart);
+  }
 };
 
-const handleCartUpdate = (updatedCart) => {
-  cart = updatedCart;
-}
+// Función para eliminar el carrito
+const removeShoppingCart = () => {
+  cart.value = [];
+};
+
+// Llama a loadLocalCart cuando el componente se monta
+onMounted(() => {
+  loadLocalCart();
+});
+
+const sendOrder = async () => {
+  try {
+    const userName = 'Juan'; // Nombre fijo para simplificar
+    const cartIds = cart.value.map(item => ({ id: item.id, amount: item.quantity }));
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user: userName,
+        products: cartIds
+      })
+    }
+    const response = await fetch("http://localhost:3000/api/orders", requestOptions);
+    if (response.ok && response.status === 201) {
+      alert('Pedido realizado con éxito');
+      removeShoppingCart();
+    } else {
+      throw ('Error al realizar la petición.')
+    }
+  } catch (error) {
+    console.error('Error al realizar la petición: ', error);
+    alert('Error al realizar el pedido');
+  }
+};
+
+
 </script>
 
 <style scoped>
@@ -41,8 +93,10 @@ const handleCartUpdate = (updatedCart) => {
   margin-bottom: 20px;
 }
 
-.go-to-cart-btn {
+.go-to-cart-btn,
+.send-order-btn {
   margin-bottom: 5px;
+  margin-right: 5px;
   padding: 10px;
   background-color: #007bff;
   color: white;
@@ -51,7 +105,39 @@ const handleCartUpdate = (updatedCart) => {
   cursor: pointer;
 }
 
-.go-to-cart-btn:hover {
+.go-to-cart-btn:hover,
+.send-order-btn:hover {
   background-color: #0056b3;
+}
+
+.remove-cart-btn {
+  margin-top: 10px;
+  padding: 10px;
+  background-color: #dc3545;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.remove-cart-btn:hover {
+  background-color: #c82333;
+}
+
+/* Estilos para el campo de entrada */
+.inputUser {
+  width: 300px;
+  padding: 10px;
+  margin-bottom: 20px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+/* Estilos para el mensaje de bienvenida */
+.welcome-message {
+  font-size: 18px;
+  font-weight: bold;
+  color: #007bff;
 }
 </style>
--- proyecto_vue/src/components/Cart.vue
+++ proyecto_vue/src/components/Cart.vue
@@ -1,168 +1,167 @@
 <template>
-    <div class="cart">
-        <button @click="goBackToProducts">Volver a la lista de productos</button>
-        <h2>Carrito de Compras</h2>
-        <p v-if="!cart.length">No hay productos en el carrito.</p>
-        <table class="cart-table">
-            <thead>
-                <tr>
-                    <th style="width: 200px;">Nombre</th>
-                    <th style="width: 100px;">Precio</th>
-                    <th style="width: 150px;">Cantidad</th>
-                    <th style="width: 100px;">Total</th>
-                    <th style="width: 150px;">Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(item, index) in cart" :key="index">
-                    <td style="width: 200px;">{{ item.name }}</td>
-                    <td style="width: 100px;">{{ item.price }}</td>
-                    <td class="quantity" style="width: 150px;">
-                        <button @click="decrementQuantity(index)">-</button>
-                        <input type="number" v-model.number="item.quantity" min="0" @blur="checkQuantity(index)"
-                            @input="checkInput(index)" />
-                        <button @click="incrementQuantity(index)">+</button>
-                    </td>
-                    <td style="width: 100px;">{{ item.price * item.quantity }}</td>
-                    <td class="actions" style="width: 150px;">
-                        <button @click="removeFromCart(index)">Eliminar</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <div class="cart-total">
-            <p>Total del Pedido: {{ total }}</p>
-            <button @click="emptyCart">Vaciar Carrito</button>
-            <button @click="clearLocalStorage">Eliminar Carrito Almacenado</button>
-        </div>
+  <div class="cart">
+    <!-- Botón para volver a la lista de productos -->
+    <button @click="goBackToProducts">Volver a la lista de productos</button>
+    <h2>Carrito de Compras</h2>
+    <!-- Mostrar un mensaje si el carrito está vacío -->
+    <p v-if="!cart.length">No hay productos en el carrito.</p>
+    <!-- Tabla para mostrar los elementos del carrito -->
+    <table class="cart-table">
+      <thead>
+        <tr>
+          <th style="width: 200px;">Nombre</th>
+          <th style="width: 100px;">Precio</th>
+          <th style="width: 150px;">Cantidad</th>
+          <th style="width: 100px;">Total</th>
+          <th style="width: 150px;">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- Iterar sobre los elementos del carrito -->
+        <tr v-for="(item, index) in cart" :key="index">
+          <td style="width: 200px;">{{ item.name }}</td>
+          <td style="width: 100px;">{{ item.price }}</td>
+          <td class="quantity" style="width: 150px;">
+            <!-- Botones para incrementar y decrementar la cantidad -->
+            <button @click="decrementQuantity(index)">-</button>
+            <input type="number" v-model.number="item.quantity" min="0" @blur="checkQuantity(index)"
+              @input="checkInput(index)" />
+            <button @click="incrementQuantity(index)">+</button>
+          </td>
+          <td style="width: 100px;">{{ (item.price * item.quantity).toFixed(2) }}</td>
+          <td class="actions" style="width: 150px;">
+            <!-- Botón para eliminar el producto del carrito -->
+            <button @click="removeFromCart(item)">Eliminar</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- Mostrar el total del pedido y botones para vaciar el carrito y eliminar el carrito almacenado -->
+    <div class="cart-total">
+      <p>Total del Pedido: {{ total.toFixed(2) }}</p>
     </div>
+  </div>
 </template>
-  
+
 <script setup>
 import { computed, defineProps, defineEmits, onMounted } from 'vue';
 
-const emit = defineEmits(['back-to-products', 'update-cart']);
+const props = defineProps(['cart']);
 
-const props = defineProps({
-    cart: {
-        type: Array,
-        default: () => [],
-    },
-});
+const emit = defineEmits(['back-to-products', 'empty-cart']);
 
+// Calcular el total del carrito
 const total = computed(() => {
-    return props.cart.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+  return props.cart.reduce((acc, item) => acc + (item.price * item.quantity), 0);
 });
 
-const removeFromCart = (index) => {
-    props.cart.splice(index, 1);
-    saveCartToLocalStorage();
-};
-
-const emptyCart = () => {
-    props.cart = [];
-    saveCartToLocalStorage();
-};
-
+// Función para volver a la lista de productos
 const goBackToProducts = () => {
-    emit('back-to-products');
+  emit('back-to-products');
 };
 
+// Decrementar la cantidad de un producto en el carrito
 const decrementQuantity = (index) => {
-    if (props.cart[index].quantity > 0) {
-        props.cart[index].quantity--;
-        if (props.cart[index].quantity === 0) {
-            removeFromCart(index);
-        }
-        saveCartToLocalStorage();
+  if (props.cart[index].quantity > 0) {
+    props.cart[index].quantity--;
+    if (props.cart[index].quantity === 0) {
+      removeFromCart(props.cart[index]);
     }
+    saveCartToLocalStorage(props.cart);
+  }
 };
 
+// Incrementar la cantidad de un producto en el carrito
 const incrementQuantity = (index) => {
-    props.cart[index].quantity++;
-    saveCartToLocalStorage();
+  props.cart[index].quantity++;
+  saveCartToLocalStorage(props.cart);
 };
 
+// Verificar la cantidad de un producto en el carrito
 const checkQuantity = (index) => {
-    if (props.cart.length > 0 && props.cart[index].quantity <= 0) {
-        removeFromCart(index);
-    }
+  if (props.cart.length > 0 && props.cart[index].quantity <= 0) {
+    removeFromCart(props.cart[index]);
+  }
 };
 
+// Verificar la entrada de cantidad de un producto en el carrito
 const checkInput = (index) => {
-    if (props.cart.length > 0 && props.cart[index].quantity <= 0) {
-        removeFromCart(index);
-    }
+  if (props.cart.length > 0 && props.cart[index].quantity <= 0) {
+    removeFromCart(props.cart[index]);
+  }
 };
 
-const saveCartToLocalStorage = () => {
-    try {
-        const serializedCart = JSON.stringify(props.cart);
-        localStorage.setItem('cart', serializedCart);
-    } catch (error) {
-        console.error('Error saving cart to local storage:', error);
-    }
+// Guardar el carrito en el almacenamiento local
+const saveCartToLocalStorage = (cart) => {
+  try {
+    const serializedCart = JSON.stringify(cart);
+    localStorage.setItem('cart', serializedCart);
+  } catch (error) {
+    console.error('Error saving cart to local storage:', error);
+  }
 };
 
-
-const clearLocalStorage = () => {
-    localStorage.removeItem('cart');
+// Eliminar el carrito almacenado en el almacenamiento local
+const clearCart = () => {
+  props.cart = [];
+  localStorage.removeItem('cart');
 };
 
+// Eliminar un producto del carrito
+const removeFromCart = (product) => {
+  const index = props.cart.findIndex(item => item.id === product.id);
+  if (index !== -1) {
+    props.cart.splice(index, 1);
+    saveCartToLocalStorage(props.cart);
+  }
+};
+
+// Cargar el carrito desde el almacenamiento local al montar el componente
 onMounted(() => {
-    try {
-        const savedCart = localStorage.getItem('cart');
-        if (savedCart) {
-            const parsedCart = JSON.parse(savedCart);
-            if (Array.isArray(parsedCart)) {
-                props.cart = parsedCart;
-            } else {
-                console.error('Error: Invalid cart data found in localStorage');
-            }
-        } else {
-            props.cart = []; // Ensure an empty array as default
-        }
-    } catch (error) {
-        console.error('Error loading cart from local storage:', error);
+  try {
+    const savedCart = localStorage.getItem('cart');
+    if (savedCart) {
+      const parsedCart = JSON.parse(savedCart);
+      if (Array.isArray(parsedCart)) {
+        props.cart = parsedCart;
+      } else {
+        console.error('Error: Invalid cart data found in localStorage');
+      }
     }
+  } catch (error) {
+    console.error('Error loading cart from local storage:', error);
+  }
 });
-
-
-const handleUpdateCart = (updatedCart) => {
-    props.cart = updatedCart;
-    saveCartToLocalStorage(); // Update local storage as well
-};
 </script>
-  
+
 <style scoped>
 .cart-table {
-    width: 100%;
-    border-collapse: collapse;
+  width: 100%;
+  border-collapse: collapse;
 }
 
 .cart-table th,
 .cart-table td {
-    border: 1px solid #ddd;
-    padding: 8px;
-    text-align: left;
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
 }
 
 .cart-table th {
-    background-color: #f2f2f2;
+  background-color: #f2f2f2;
 }
 
 .cart-total {
-    border-top: 1px solid #ccc;
-    padding: 10px;
-    text-align: right;
+  border-top: 1px solid #ccc;
+  padding: 10px;
+  text-align: right;
 }
 
 .cart-table .actions {
-    text-align: center;
+  text-align: center;
 }
 
 .cart-table .quantity {
-    text-align: center;
+  text-align: center;
 }
 </style>
-  
